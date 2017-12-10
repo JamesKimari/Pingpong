76808487d66eea79ee62ec932d2e595603e3a321
JamesKimari <jamesgkimari@gmail.com>
2017-12-10 20:47:00 +0300

    added if and else if loops to determine when to output ping, pong, pingpong or return the userinput

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ var pingPonged = []
 
 function pingPongIt(inputNumber) {
   for (var number = 1; number <= inputNumber; number++) {
+    if (number % 15 === 0) {
+      pingPonged.push("pingpong")
+    } else if (number % 3 === 0) {
+      pingPonged.push("ping")
+    } else if (number % 5 === 0) {
+      pingPonged.push("pong")
+    } else {
+      pingPonged.push(number)
+    }
+  }
+}
