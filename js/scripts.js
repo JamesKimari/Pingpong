3808eca322263dc52d96c1b3ef09f100da892755
JamesKimari <jamesgkimari@gmail.com>
2017-12-10 20:45:41 +0300

    created an array for the numbers and included a for loop to iterate through the numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,3 @@ var pingPonged = []
 
 function pingPongIt(inputNumber) {
   for (var number = 1; number <= inputNumber; number++) {
-    if (number % 15 === 0) {
-      pingPonged.push("pingpong")
-    } else if (number % 3 === 0) {
-      pingPonged.push("ping")
-    } else if (number % 5 === 0) {
-      pingPonged.push("pong")
-    } else {
-      pingPonged.push(number)
-    }
-  }
-}
-
-
-$(document).ready(function() {
-  $("form#pingPongInput").submit(function(event) {
-    $("#btnSubmit").toggle()
-    $("#btnClear").show()
-    event.preventDefault()
-    var inputNumber = parseInt($("input#inputNumber").val())
-    pingPongIt(inputNumber)
-    pingPonged.forEach(function(inputNumber) {
-      $("#results").append("<li>" + inputNumber + "</li>").addClass('blue')
-    })
-
-    $("#btnClear").click(function() {
-      $("#results").remove()
-    })
-  })
-})
